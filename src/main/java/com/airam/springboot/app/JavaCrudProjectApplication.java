dce69665a0576f6fe47941e63217ef27c74d094b
ce <mcelestoledo@gmail.com>
2020-12-17 17:22:42 -0300

    Estructura base del proyecto

---
+++ src/main/java/com/airam/springboot/app/JavaCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.airam.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airam/springboot/app/JavaCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airam.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
