ebeda94b0531e53f9446d3b34db1882e245210b7
ce <mcelestoledo@gmail.com>
2020-12-19 16:10:48 -0300

    update models

--- src/main/java/com/airam/springboot/app/models/product.java
+++ src/main/java/com/airam/springboot/app/models/product.java
@@ -1,5 +1,32 @@
 package com.airam.springboot.app.models;
 
-public class product {
+import java.io.Serializable;
+
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class product implements Serializable {
+
+	private static final long serialVersionUID = 2267426928408897992L;
+	
+	@MongoId(value = FieldType.OBJECT_ID)
+	private String id;
+	private String name;
+	private String description;
+	private String brand;
+	private Double price; 
+	private Integer quantity;
+	private String category;
+	
+	
+	
 
 }
